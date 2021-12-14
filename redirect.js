4cc507aaa232a1ec69602e60e143674dc3b5dc0c
Kanakun <kanakun2007@gmail.com>
2021-12-13 16:58:36 -0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,9 @@
 if (getCookie("lang") == "EN") {
     location = "https://en.scuderiapacifica.com";
+} else if (getCookie("lang") == "ES") {
+    location = "https://es.scuderiapacifica.com";
+} else if (getCookie("lang") == "FR") {
+    location = "https://fr.scuderiapacifica.com";
 } else {
     $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=93da069059ca42718ec93aa9b01697e4", function (data) {
         console.log(data.country_code);
