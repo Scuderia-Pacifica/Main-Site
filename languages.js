ebe266da4ca5a62f4afcf4c3750c8d6625e35250
Kanakun <kanakun2007@gmail.com>
2021-12-13 16:27:58 -0800

    Update languages.js

--- languages.js
+++ languages.js
@@ -21,18 +21,13 @@ function getCookie(cname) {
     return "";
 }
 
-if (getCookie("lang") == null) {
-    console.log("test");
-    $.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=93da069059ca42718ec93aa9b01697e4", function (data) {
-        console.log(data.country_code);
-        if (data.country_code == "ES") {
-            location = "https://es.scuderiapacifica.com";
-        } else if (data.country_code == "FR") {
-            location = "https://fr.scuderiapacifica.com";
-        } else {
-            location = "https://en.scuderiapacifica.com";
-        }
-    })
-}else {
-    console.log(getCookie("lang"));
-}
+$.getJSON("https://ipgeolocation.abstractapi.com/v1/?api_key=93da069059ca42718ec93aa9b01697e4", function (data) {
+    console.log(data.country_code);
+    if ((data.country_code == "ES") || (getCookie("lang") == EN)) {
+        location = "https://es.scuderiapacifica.com";
+    } else if (data.country_code == "FR") {
+        location = "https://fr.scuderiapacifica.com";
+    } else {
+        location = "https://en.scuderiapacifica.com";
+    }
+})
