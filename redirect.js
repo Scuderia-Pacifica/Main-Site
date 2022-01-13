0b366cca22d0e368b57c60dca4329c6e7efffb47
lego man <12103838+rwilliaise@users.noreply.github.com>
2022-01-12 17:52:16 -0800

    fix: switch to .org

--- redirect.js
+++ redirect.js
@@ -1,22 +1,22 @@
 function load() {
     if (getCookie("lang") == "EN") {
-        location = "https://en.scuderiapacifica.com";
+        location = "https://en.scuderiapacifica.org";
     } else if (getCookie("lang") == "ES") {
-        location = "https://es.scuderiapacifica.com";
+        location = "https://es.scuderiapacifica.org";
     } else if (getCookie("lang") == "FR") {
-        location = "https://fr.scuderiapacifica.com";
+        location = "https://fr.scuderiapacifica.org";
     } else {
         const nav = navigator.language
 
         switch (nav) {
             case "es":
-                location = "https://es.scuderiapacifica.com"
+                location = "https://es.scuderiapacifica.org"
                 break
             case "fr":
-                location = "https://fr.scuderiapacifica.com"
+                location = "https://fr.scuderiapacifica.org"
                 break
             default:
-                location = "https://en.scuderiapacifica.com"
+                location = "https://en.scuderiapacifica.org"
                 break
         }
     }
