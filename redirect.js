f70e94b95f307f19f2bce152f81a56afde13a1ad
Kanakun <kanakun2007@gmail.com>
2022-03-17 16:34:52 -0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,23 +1,24 @@
 function load() {
-    if (getCookie("lang") == "EN") {
-        location = "https://en.scuderiapacifica.org";
-    } else if (getCookie("lang") == "ES") {
-        location = "https://es.scuderiapacifica.org";
-    } else if (getCookie("lang") == "FR") {
-        location = "https://fr.scuderiapacifica.org";
-    } else {
-        const nav = navigator.language
+    // if (getCookie("lang") == "EN") {
+    //     location = "https://en.scuderiapacifica.org";
+    // } else if (getCookie("lang") == "ES") {
+    //     location = "https://es.scuderiapacifica.org";
+    // } else if (getCookie("lang") == "FR") {
+    //     location = "https://fr.scuderiapacifica.org";
+    // } else {
+    //     const nav = navigator.language
 
-        switch (nav) {
-            case "es":
-                location = "https://es.scuderiapacifica.org"
-                break
-            case "fr":
-                location = "https://fr.scuderiapacifica.org"
-                break
-            default:
-                location = "https://en.scuderiapacifica.org"
-                break
-        }
-    }
+    //     switch (nav) {
+    //         case "es":
+    //             location = "https://es.scuderiapacifica.org"
+    //             break
+    //         case "fr":
+    //             location = "https://fr.scuderiapacifica.org"
+    //             break
+    //         default:
+    //             location = "https://en.scuderiapacifica.org"
+    //             break
+    //     }
+    // }
+    location = "https://en.scuderiapacifica.org"
 }
